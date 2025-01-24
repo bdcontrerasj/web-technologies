3ed909138bb015519c06e59d0c4e31b3ab38dc67
Gshi3583 <Gshi3583@conestogac.on.ca>
2025-01-24 16:54:02 -0500

    new branch and login page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar'; // Asegúrate de que la ruta sea correcta
-import  {Shop} from './pages/shop/shop';
-import  {Cart} from './pages/cart/cart';
-import { ShopContextProvider } from './context/shop-context';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar"; // Asegúrate de que la ruta sea correcta
+import Login from "./pages/login/login";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
 // Componentes de ejemplo para las rutas
-
 
 function App() {
   return (
     <div className="App">
       <ShopContextProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Shop />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </Router>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
       </ShopContextProvider>
     </div>
   );
 }
 
 export default App;
-
-
